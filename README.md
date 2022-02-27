f0268de6e1e253aa2bd0f2338abb88bd6ab15b62
youcefs21 <youcefs2121@gmail.com>
2022-02-27 12:42:58 -0500

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+ # Card Game Bot
+
+
+ a discord bot that allows you to play a variety of card games with friends
---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+import os
+from decouple import config
+
+bot = discord.Bot()
+
+@bot.slash_command()
+async def hello(ctx, name: str = None):
+    name = name or ctx.author.name
+    await ctx.respond(f"Hello {name}!")
+
+
+bot.run(config('TOKEN'))
