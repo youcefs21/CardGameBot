a17bb0da53bfd62820102692680d2be5c1aee41d
youcefs21 <youcefs2121@gmail.com>
2022-02-27 17:10:48 -0500

    add global state

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
 import os
 from decouple import config
+from collections import defaultdict
 
 bot = discord.Bot(debug_guilds=[config('DEBUG_GUILD', cast=int)])
+players = defaultdict(lambda: None) # a dict of the form {userId: gameId}
+games = {} # a dict of the form {gameId: (deckId, turnCount, [list of players], {gameSpecificArgs})}
 
 # loading all the extensions
 for filename in os.listdir('./cogs'):
