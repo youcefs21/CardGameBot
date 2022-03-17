a7c62276ac89e8ad903f58b0c352be71132e2dfd
youcefs21 <youcefs2121@gmail.com>
2022-03-17 09:18:33 -0400

    president game service uniqueness check

--- cogs/GameService.py
+++ cogs/GameService.py
@@ -1,18 +1,36 @@
+import discord
 from discord.ext import commands
 from discord.commands import SlashCommandGroup
 import games.PlayPresident as PlayPresident
+from typing import Set
 
 
 class GameService(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: discord.Bot):
         self.bot = bot
+        self.users_in_game: Set[int] = set()
 
     play = SlashCommandGroup("play", "play a card game!")
 
     @play.command(description="a game of president")
-    async def president(self, ctx):
+    async def president(self, ctx: discord.ApplicationContext):
+
+        user_id = ctx.user.id
+        if user_id in self.users_in_game:
+            await ctx.respond("You're already in a game", ephemeral=True)
+            return
+
+        self.users_in_game.add(user_id)
+
+        # lobby
+
+        # role choice and last chance to quit game
+
+        # pre-game card trade and start of rounds
         await PlayPresident.president(ctx, self.bot)
+
+        # game summary
 
 
 def setup(bot):
